f30d5deaeb3da28b442b6b0d5fa87ea07ff2b205
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-07-13 23:46:00 -0600

    fix(client): emote exports

--- modules/client.lua
+++ modules/client.lua
@@ -3,7 +3,7 @@
 -- Play Emote --
 function XTEmote(emote)
     if emote == nil then XTDebug('Play Emote Error', 'Emote is nil!') return end
-    if not exports.scully_emotemenu:PlayByCommand(emote) then
+    if not exports.scully_emotemenu:playEmoteByCommand(emote) then
         TriggerEvent('animations:client:EmoteCommandStart', {emote})
     end
     XTDebug('Play Emote', emote)
@@ -11,7 +11,7 @@ end
 
 -- End Emote --
 function XTEndEmote()
-    if not exports.scully_emotemenu:CancelAnimation() then
+    if not exports.scully_emotemenu:cancelEmote() then
         TriggerEvent('animations:client:EmoteCommandStart', {'c'})
     end
     XTDebug('End Emote')
